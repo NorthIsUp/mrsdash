e0a33010de388d9c9893e33c974a6724ac494600
Adam Hitchcock <adam@northisup.com>
2012-02-29 00:13:22 -0800

    red and yellow!

---
+++ pavement.py
@@ -0,0 +1,54 @@
+from paver.easy import *
+import pkg_resources
+from itertools import chain
+
+
+@task
+def kpyc():
+    sh('find . -iname "*.pyc" -delete')
+
+
+@task
+@needs(['kpyc'])
+def clean():
+    print "rming the build dir and *.egg-info"
+    delete = (
+        (path(__file__).dirname().joinpath("build"), ),
+        path(__file__).dirname().glob("*.egg-info"),
+        )
+
+    for d in chain(*delete):
+        path(d).rmtree()
+
+
+@task
+@needs(['clean'])
+@cmdopts([
+    ('module=', 'm', 'module to get the version from')
+])
+def publish(options):
+    """Publish <module> to the disqus pip cheeze thingy"""
+    if not hasattr(options, 'module'):
+        print "module is not an option, it is required"
+
+    sh("python setup.py build")
+    sh("python setup.py install")
+
+    VERSION = pkg_resources.get_distribution(options.module).version
+    print VERSION
+
+    print "creating tag " + VERSION
+    sh("git tag " + VERSION, ignore_error=True)
+
+    print "pushing tag"
+    sh("git push origin " + VERSION)
+
+    manifest = path('MANIFEST')
+    if manifest.exists():
+        print "removing manifest file"
+        manifest.remove()
+
+    print "uploading package."
+    sh("python setup.py sdist upload -r disqus")
+
+    clean()
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-__version__ = '0.1.12'
+__version__ = '0.1.0'
 
 tests_require = [
     'nose',
